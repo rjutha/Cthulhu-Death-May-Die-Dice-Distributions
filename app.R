03655d0f09eaead99364db06e6dc6b5eef466229
rjutha <rahimjuthaguy@gmail.com>
2023-06-27 18:11:31 -0700

    Added Files

---
+++ app.R
@@ -0,0 +1,112 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(highcharter)
+library(tidyverse)
+source("cthulhu dice.R")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Cthulhu Death May Die Dice Distribution"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+          selectInput(inputId = "rolls",
+                         label = "Number of Simulated Rolls",
+                         choices = c("100" = 100,
+                                     "1,000" = 1000,
+                                     "10,000" = 10000,
+                                     "100,000" = 100000),
+                         selected = 10000),
+            selectInput(inputId = "black",
+                        label = "Number of black dice to roll",
+                        choices = c(0,
+                                    1,
+                                    2,
+                                    3,
+                                    4,
+                                    5,
+                                    6,
+                                    7,
+                                    8,
+                                    9),
+                        selected = 3),
+            selectInput(inputId = "green",
+                        label = "Number of green dice to roll",
+                        choices = c(0,
+                                    1,
+                                    2,
+                                    3,
+                                    4,
+                                    5,
+                                    6,
+                                    7,
+                                    8,
+                                    9),
+                        selected = 0)
+        ),
+        
+
+        # Show a plot of the generated distribution
+        mainPanel(
+          highchartOutput("hc_plot")
+           # plotOutput("distPlot")
+          
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  
+
+rerun_data <- reactive({
+  
+      roll_n(as.numeric(input$rolls),input$black, input$green) %>%
+      as_tibble() %>%
+      select(-probability) %>%
+      group_by_all() %>%
+      count(name = "count") %>%
+      ungroup() %>%
+      arrange(-count) %>%
+      mutate(dist = count / as.numeric(input$rolls))
+  })
+
+  # output$distPlot <- renderPlot({
+  #    df <- rerun_data()
+  #       # generate bins based on input$bins from ui.R
+  #       # draw the histogram with the specified number of bins
+  #       print(plot(x = df$dist))
+  #   })
+
+  output$hc_plot <- renderHighchart({
+    df <- rerun_data()
+    hchart(df,
+           "column",
+           hcaes(x = (1:length(dist)), y = dist),
+           color = "#478A54") %>%
+      hc_plotOptions(
+        column = list(
+          pointPadding = 0,
+          borderWidth = 0,
+          groupPadding = 0,
+          shadow = FALSE
+        )
+      )
+  })
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ cthulhu dice.R
@@ -0,0 +1,133 @@
+library(tidyverse)
+# https://datascienceplus.com/strategies-to-speedup-r-code/
+
+# Black Die
+# 1 Blank
+# 2 ! (success)
+# 1 tentacle
+# 1 star + tentacle
+# 1 tentacle + ! (sucess)
+
+# Green Die
+  # 2 Blank
+  # 2 ! (success)
+  # 1 star
+  # 1 star + ! (success)
+
+roll_dice <- function(black, green, message = TRUE){
+  success <- 0
+  star <- 0
+  tentacle <- 0
+  probability <- 1
+  
+  if(black > 0){
+    for(i in 1:black){
+      roll <- sample(1:6, 1)
+      # Blank
+      if(roll == 1){
+        if (message) print("Blank")
+        probability = probability * (1/6)
+      }
+      # Success
+      else if(roll %in% c(2,3)){
+        success = success + 1
+        probability = probability * (2/6)
+        if (message) print("Success")
+      }
+      # Tentacle
+      else if(roll == 4){
+        tentacle = tentacle + 1
+        probability = probability * (1/6)
+        if (message) print("Tentacle")
+      }
+      # Star + Tentacle
+      else if(roll == 5){
+        star = star + 1
+        tentacle = tentacle + 1
+        probability = probability * (1/6)
+        if (message) print("Star + Tentacle")
+      }
+      # Success + Tentacle
+      else if(roll == 6){
+        success = success + 1
+        tentacle = tentacle + 1
+        probability = probability * (1/6)
+        if (message) print("Success + Tentacle")
+      }
+    }
+  }
+  
+  if(green > 0 ){
+    for(i in 1:green){
+      roll <- sample(1:6, 1)
+      # Blanks
+      if(roll %in% c(1,2)){
+        if (message) print("Blank")
+        probability = probability * (2/6)
+      }
+      # Success
+      else if(roll %in% c(3,4)){
+        if (message) print("Success")
+        success = success + 1
+        probability = probability * (2/6)
+      }
+      # Star
+      else if(roll == 5){
+        if (message) print("Star")
+        star = star + 1
+        probability = probability * (1/6)
+      }
+      # Star + Success
+      else if(roll == 6){
+        if (message) print("Success + Star")
+        success = success + 1
+        star = star + 1
+        probability = probability * (1/6)
+      }
+    }
+  }
+  
+  return(
+    c(
+      success = success,
+      star = star,
+      tentacle = tentacle,
+      probability = probability
+    )
+  )
+}
+
+#roll_dice(3,0)
+
+roll_n <- function(n, black, green){
+  df <- matrix(nrow = n, ncol = 4, dimnames = list(c(), c("success", "star", "tentacle", "probability")))
+  for(i in 1:n){
+    temp <- roll_dice(black, green, message = FALSE)
+    df[i,] <- temp
+  }
+  return(df)
+}
+
+# n = 10000
+# data <- roll_n(n,3,0) %>%
+#   as_tibble() %>%
+#   select(-probability) %>%
+#   group_by_all() %>%
+#   count(name = "count") %>%
+#   ungroup() %>%
+#   arrange(-count) %>%
+#   mutate(dist = count / n)
+# 
+# hchart(data,
+#        "column",
+#        hcaes(x = 1:nrow(data), y = data$dist)) %>%
+#   hc_plotOptions(
+#     column = list(
+#       pointPadding = 0,
+#       borderWidth = 0,
+#       groupPadding = 0,
+#       shadow = FALSE
+#          )
+#        )
+# 
+#        
